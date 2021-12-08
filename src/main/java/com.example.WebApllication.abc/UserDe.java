2037c27816a56421b6e3ace34757896ddf97e80d
Varun Gupta <65545724+varungupta2000@users.noreply.github.com>
2021-12-08 11:33:22 +0530

    Create UserDe.java

---
+++ src/main/java/com.example.WebApllication.abc/UserDe.java
@@ -0,0 +1,11 @@
+package com.example.WebApplication.abc;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.WebApplication.DetailsCapture;
+
+public interface UserDe extends CrudRepository<DetailsCapture,Integer>
+{
+	
+
+}
